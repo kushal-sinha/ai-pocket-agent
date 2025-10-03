0265b9464633265801ac3f11e97dfdca5cf35b1d
kushal-sinha <kushal147258369@gmail.com>
2025-10-04 00:47:10 +0530

    added aunthentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
 
+
+
 export default function RootLayout() {
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ClerkProvider>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,83 @@ import LottieView from "lottie-react-native";
 import { Dimensions, Platform, StyleSheet, Text, TouchableOpacity, View, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
+import { useAuth, useSSO, useUser } from "@clerk/clerk-expo";
+import { useCallback, useEffect } from "react";
+import * as WebBrowser from 'expo-web-browser';
+import { router, useRouter } from "expo-router";
+import * as AuthSession from 'expo-auth-session';
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Logic to warm up the browser goes here
+    void WebBrowser.warmUpAsync();
+    return () => {
+      // Logic to cool down the browser goes here
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+}
+
+WebBrowser.maybeCompleteAuthSession();
+
 
 export default function Index() {
+
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+  const { user } = useUser();
+  console.log(user?.primaryEmailAddress?.emailAddress);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      // redirect to home screen
+    }
+  }, [isSignedIn])
+
+
+  useWarmUpBrowser()
+
+  // Use the `useSSO()` hook to access the `startSSOFlow()` method
+  const { startSSOFlow } = useSSO()
+
+  const onLoginPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
+        strategy: 'oauth_google',
+        // For web, defaults to current path
+        // For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+        // For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+        redirectUrl: AuthSession.makeRedirectUri(),
+      })
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        setActive!({
+          session: createdSessionId,
+          // Check for session tasks and navigate to custom UI to help users resolve them
+          // See https://clerk.com/docs/guides/development/custom-flows/overview#session-tasks
+          navigate: async ({ session }) => {
+            if (session?.currentTask) {
+              console.log(session?.currentTask)
+              router.push('/sign-in/tasks')
+              return
+            }
+
+            router.push('/')
+          },
+        })
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // See https://clerk.com/docs/guides/development/custom-flows/authentication/oauth-connections#handle-missing-requirements
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }, [])
   return (
     <LinearGradient
       colors={["#0A1D37", "#1E3C72", "#2A5298"]}
@@ -34,7 +109,7 @@ export default function Index() {
           Try it today, completely free!
         </Text>
 
-        <TouchableOpacity activeOpacity={0.8} style={styles.buttonWrapper}>
+        <TouchableOpacity onPress={onLoginPress} activeOpacity={0.8} style={styles.buttonWrapper}>
           <LinearGradient
             colors={["#36D1DC", "#5B86E5"]}
             start={{ x: 0, y: 0 }}
